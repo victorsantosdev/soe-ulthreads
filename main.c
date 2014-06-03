6f506ae2a101a4f0536435d7426fbc34a21736f9
victorsantos <victor.ifsc@gmail.com>
2014-06-03 09:51:08 -0300

    main funcionando, falta implementar preempcao e thread idle

--- main.c
+++ main.c
@@ -1,24 +1,34 @@
 #include "ulthread.h"
 
+/*generic thread, just for test purpose*/
+/*print int a, <a> times */
 void thr_run(int a) {
-    unsigned int turn = 0;
-    while (1) {
+    
+    int times = a;
+    while (times--) {
         printf("%d\n", a);
+        /*free cpu to run another thread with yield method()*/
         yield();
     }
-    finish(0);
+    finish(a);
 }
 
 int main(void) {
 
     init();
+    
+    int res_a, res_b;
+
     int tid_a = create(&thr_run, 10);
     int tid_b = create(&thr_run, 15);
 
-    while(1) {
-        printf("A\n");
-        yield();
-    }
+    join(tid_a, &res_a);
+    /*waiting until thread a returns*/
+    join(tid_b, &res_b);
+
+   printf("Both threads have finished with status %d and %d\n", res_a, res_b);
+   printf("I'm donw too... bye bye!\n");
+
 
     return 0;
 }
